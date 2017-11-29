a3a96f187273f8d9b5d4e07c38b8fe4c02280748
Michael Morandi <michaelm.dev@gmx.net>
2017-11-29 07:43:20 +0100

    Threading in web app

--- web.py
+++ web.py
@@ -1,4 +1,7 @@
 import json
+import time
+
+import threading
 
 from flask import Flask
 from flask import Response
@@ -13,19 +16,41 @@ app = Flask(__name__)
 
 adsb_config = Config('config.json')
 bs_db = BaseStationDB(adsb_config.data_folder + "BaseStation.sqb")
-mm2 = ModeSMixer(adsb_config.host, adsb_config.port)
-mil_ranges = MilRanges(adsb_config.data_folder)
+
+class UpdaterThread(threading.Thread):
+
+    def __init__(self, config):
+
+        threading.Thread.__init__(self)
+        self.mm2 = ModeSMixer(config.host, config.port)
+        self.mil_ranges = MilRanges(config.data_folder)
+        self.interrupted = False
+
+    def run(self):
+
+        while not self.interrupted:
+            print("hello from thread %s, " % threading.get_ident())
+            time.sleep(2)
+
+        print("interupted")
+
+t = UpdaterThread(adsb_config)
+t.start()
+#print(threading.get_ident())
 
 @app.route("/")
 def hello():
 
+
+    t.interrupted = True
+
     response = []
 
-    for icao24 in mm2.query_live_aircraft():
+    #for icao24 in mm2.query_live_aircraft():
 
-        aircraft = bs_db.query_aircraft(icao24)
+     #   aircraft = bs_db.query_aircraft(icao24)
 
-        response.append(str(aircraft))
+     #   response.append(str(aircraft))
 
     print(response)
     return Response(json.dumps(response), mimetype='application/json')
