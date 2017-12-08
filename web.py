3f2d21a335a12a6730e0c94196c254b94d6a48c4
Michael Morandi <michaelm.dev@gmx.net>
2017-12-08 20:57:11 +0100

    Added Overall map

--- web.py
+++ web.py
@@ -58,10 +58,38 @@ def get_positions(icao24):
     else:
         return "Not found", 404
 
+@app.route("/pos/<icao24>") 
+def get_position(icao24):
+
+    entries = updater.get_entry(icao24)
+
+    if entries:
+        return Response(json.dumps(entries.pos), mimetype='application/json')
+    else:
+        return "Not found", 404
+
+@app.route("/positions") 
+def get_all_positions():
+
+    entries = updater.get_active_entries()
+
+    result = []
+    for entry in entries:
+        result.append( [entry[0],entry[1].pos])
+
+    if entries:
+        return Response(json.dumps(result), mimetype='application/json')
+    else:
+        return "Not found", 404
 
 @app.route("/map/<icao24>") 
 def get_map(icao24):
     return render_template('map.html', icao24=icao24)
+
+@app.route("/map") 
+def get_map_all():
+    return render_template('map.html', icao24='')
+
 
 @app.after_request
 def after_request(response):
