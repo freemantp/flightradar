f7c66ee7c4ea4d270b7631d2c586472cca200d6b
Michael Morandi <michaelm.dev@gmx.net>
2018-07-22 16:16:21 +0200

    Updated db models

--- adsb/db/dbmodels.py
+++ adsb/db/dbmodels.py
@@ -1,19 +1,31 @@
 import peewee as pw
 import datetime
 
-database_proxy = pw.Proxy() 
+database_proxy = pw.Proxy()
+
+class Flight(pw.Model):
+    id = pw.AutoField
+    callsign = pw.CharField
+    modeS = pw.FixedCharField(max_length=6)
+    archived = pw.BooleanField(default=False)
+
+    class Meta:
+        database = database_proxy
+
+    def __repr__(self):
+        return 'Flight id={:d} callsign={:s} modeS={:s} archived={:s]'.format( self.id, self.callsign, self.modeS, self.archived )
 
 class Position(pw.Model):
-    icao = pw.FixedCharField(max_length=6)
+    flight_fk = pw.ForeignKeyField(Flight)
     lat = pw.FloatField()
     lon = pw.FloatField()
     alt = pw.IntegerField(null=True)
     timestmp = pw.DateTimeField(default=datetime.datetime.utcnow)
-    archived = pw.BooleanField(default=False)
 
     class Meta:
-        primary_key = pw.CompositeKey('icao', 'timestmp')
         database = database_proxy
 
     def __repr__(self):
-        return 'Position {:s} ({:f},{:f},{:d}) {:s}'.format( self.icao, self.lat, self.lon, self.alt, str(self.timestmp) )
+        return 'flight={:d} pos=({:f},{:f},{:d}) at={:s}'.format( self.flight_fk, self.lat, self.lon, self.alt, str(self.timestmp) )
+
+MODELS = [Flight, Position]
