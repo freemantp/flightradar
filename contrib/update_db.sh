61dd80243e925fa7bc5c857a7324fd106c4be3ce
Michael Morandi <michaelm.dev@gmx.net>
2019-04-17 19:29:41 +0000

    Fixed paths in restart script

--- contrib/update_db.sh
+++ contrib/update_db.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
 service modesmixer2 stop
 sleep 5
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+dbfile="$(dirname $SCRIPT_DIR)/resources/BaseStation.sqb"
+
 rm /usr/share/modesmixer2/BaseStation2.sqb
 mv /usr/share/modesmixer2/BaseStation.sqb /usr/share/modesmixer2/BaseStation2.sqb
-cp /home/pi/adsb-playground/resources/BaseStation.sqb /usr/share/modesmixer2/BaseStation.sqb
+cp $dbfile /usr/share/modesmixer2/BaseStation.sqb
 chown modesmixer:modesmixer /usr/share/modesmixer2/BaseStation.sqb
 chmod 644 /usr/share/modesmixer2/BaseStation.sqb
 service modesmixer2 start
