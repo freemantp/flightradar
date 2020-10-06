17bd13ec9523599c183486f30d32f57bf2eb158b
Michael Morandi <michaelm.dev@gmx.net>
2020-10-06 23:42:35 +0200

    Updoate dependencies, minor tweaks

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.8-alpine 
+FROM python:3.8.6-alpine 
 
 LABEL maintainer="Michael Morandi"
 
--- flightradar.py
+++ flightradar.py
@@ -1,7 +1,7 @@
 from app import create_app
 
 app = create_app()
-@app.cli.command()
+@app.cli.command("initschema")
 def initschema():
     """Initialize DB schema."""
 
